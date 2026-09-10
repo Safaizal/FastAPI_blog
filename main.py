d696991464b047a514bdd4aea270f8264592023e
Safaizal <safaizalways@gmail.com>
2026-09-10 12:39:33 +0530

    Added validation and Error handling

--- main.py
+++ main.py
@@ -1,37 +1,110 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 # it tells where to find the html pages
 templates = Jinja2Templates(directory="templates")
-#this is to tell to find css styles
+# this is to tell to find css styles
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 posts: list[dict] = [
-        {
-            "id": 1,
-            "author": "faizal",
-            "title": "Fastapi Framework",
-            "content": "This is an opensourced easy framework",
-            "date_posted": "september 2, 2025"
-            },
-        {
-            "id": 2,
-            "author": "faizal",
-            "title": "Fastapi2 Framework",
-            "content": "This is an opensourced easy framework",
-            "date_posted": "september 2, 2025"
-            }
-        ]
+    {
+        "id": 1,
+        "author": "faizal",
+        "title": "Fastapi Framework",
+        "content": "This is an opensourced easy framework",
+        "date_posted": "september 2, 2025",
+    },
+    {
+        "id": 2,
+        "author": "faizal",
+        "title": "Fastapi2 Framework",
+        "content": "This is an opensourced easy framework",
+        "date_posted": "september 2, 2025",
+    },
+]
 
-#To give route we use decorators like in flask
+
+# To give route we use decorators like in flask
 @app.get("/", include_in_schema=False, name="home")
 @app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"})
+    return templates.TemplateResponse(
+        request, "home.html", {"posts": posts, "title": "Home"}
+    )
+
+
+@app.get("/posts/{post_id}", include_in_schema=False) # by doing this we can seperate the json outputs and user side outputs, wedon't want user to see json and get confused
+def post_page(request: Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            title = post["title"][:50]
+            return templates.TemplateResponse(
+                request, "posts.html", {"post": post, "title": title}
+            )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="page_not_found")
+
 
 @app.get("/api/posts")
 def get_posts():
     return posts
 
+
+@app.get("/api/posts/{post_id}")  # this kind of thing is called path parameters
+def get_post(
+    post_id: int,
+):  # here due to the :int fastapi automatically handles the error if, post_id is not integers send us a status code.
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="page_not_found")
+
+
+@app.exception_handler(StarletteHTTPException)
+def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
+    message = (
+        exception.detail
+        if exception.detail
+        else "An error occurred. Please check your request and try again."
+    )
+
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exception.status_code,
+            content={"detail": message},
+        )
+
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": exception.status_code,
+            "title": exception.status_code,
+            "message": message,
+        },
+        status_code=exception.status_code, # if don't pass this then all the content will shows like an error
+    )
+
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exception: RequestValidationError):
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content={"detail": exception.errors()},
+        )
+
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "message": "Invalid request. Please check your input and try again.",
+        },
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+    )
