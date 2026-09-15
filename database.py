4360342eb6cbb506b711e5427be1a1951ba6cc2e
Safaizal <safaizalways@gmail.com>
2026-09-15 13:03:39 +0530

    modified the function to async

--- database.py
+++ database.py
@@ -1,20 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db" #this is where we change in order to change our database and the file mention in this line will automatically created.
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
 
-engine = create_engine(
+engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # this is factory method that creates the database sessions, each database gets it's own session
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 
 class Base(DeclarativeBase):
     pass
 
 
-def get_db():
-    with SessionLocal() as db:
-        yield db #it's generator that cleanup and match the db session to our routes
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
