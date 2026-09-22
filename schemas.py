bf255bcb2a9a0f22c8e785deba80ded9b39c899a
Safaizal <safaizalways@gmail.com>
2026-09-22 21:32:12 +0530

    Added pagination

--- schemas.py
+++ schemas.py
@@ -7,8 +7,10 @@ class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length=50)
     email: EmailStr = Field(max_length=120)
 
+
 class UserCreate(UserBase):
-    password: str = Field(min_length=8, max_length=100)
+    password: str = Field(min_length=8)
+
 
 class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
@@ -18,27 +20,34 @@ class UserPublic(BaseModel):
     image_file: str | None
     image_path: str
 
+
 class UserPrivate(UserPublic):
     email: EmailStr
+
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
+
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
 
+
 class PostCreate(PostBase):
     pass
+
 
 class PostUpdate(BaseModel):
     title: str | None = Field(default=None, min_length=1, max_length=100)
     content: str | None = Field(default=None, min_length=1)
+
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
@@ -47,3 +56,11 @@ class PostResponse(PostBase):
     user_id: int
     date_posted: datetime
     author: UserPublic
+
+
+class PaginatedPostsResponse(BaseModel):
+    posts: list[PostResponse]
+    total: int
+    skip: int
+    limit: int
+    has_more: bool
